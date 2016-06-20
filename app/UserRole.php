0f023efa0cf1dad4e37e6f546d6f5ac6fe0039ab
Alasdair Keyes <alasdair@akeyes.co.uk>
2016-06-20 09:56:07 +0100

    Set no timestamps on user_roles table model

--- app/UserRole.php
+++ app/UserRole.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class UserRole extends Model
 {
     //
+    public $timestamps = false;
+
     public function user()
     {
         return $this->belongsTo(User::class);
